b3fbd8265b9767bdbb29de950f280838fd2ad7c8
Giorgio Dramis <dramisgiorgio@outlook.com>
2026-08-10 18:29:44 +0200

    plugins: port service dialogs to GTK4
    Adapt Find My Phone, Mousepad, Notification reply, and Share dialog code to the GTK4/Adw templates and APIs.

--- src/service/plugins/mousepad.js
+++ src/service/plugins/mousepad.js
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-import Gdk from 'gi://Gdk';
+import Gdk from 'gi://Gdk?version=4.0';
 import GObject from 'gi://GObject';
 
 import * as Components from '../components/index.js';
@@ -302,6 +302,7 @@ const MousepadPlugin = GObject.registerClass({
         } else if (KeyMap.get(input.specialKey) === Gdk.KEY_BackSpace) {
             this._dialog.entry.emit('backspace');
         }
+
     }
 
     /**
